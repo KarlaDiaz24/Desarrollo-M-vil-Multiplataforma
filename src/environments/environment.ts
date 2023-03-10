d6c8393e562fef7a3fa432550876e81bdf086473
KarlaDiaz <diazmontserrath48@gmail.com>
2023-03-10 08:24:04 -0500

    Solo falta validar el login y corecciones esteticas.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,13 +6,13 @@ export const environment = {
   production: false,
   // Your web app's Firebase configuration
   firebaseConfig: {
-  apiKey: "AIzaSyDA2n88zmKHenAvvOea4fl53Ha5CLOHxq4",
-  authDomain: "hotel-de-diaz-sm52.firebaseapp.com",
-  databaseURL: "https://hotel-de-diaz-sm52-default-rtdb.firebaseio.com",
-  projectId: "hotel-de-diaz-sm52",
-  storageBucket: "hotel-de-diaz-sm52.appspot.com",
-  messagingSenderId: "496732230310",
-  appId: "1:496732230310:web:0b92ad5353eb4d79825a67"
+    apiKey: "AIzaSyDA2n88zmKHenAvvOea4fl53Ha5CLOHxq4",
+    authDomain: "hotel-de-diaz-sm52.firebaseapp.com",
+    databaseURL: "https://hotel-de-diaz-sm52-default-rtdb.firebaseio.com",
+    projectId: "hotel-de-diaz-sm52",
+    storageBucket: "hotel-de-diaz-sm52.appspot.com",
+    messagingSenderId: "496732230310",
+    appId: "1:496732230310:web:0b92ad5353eb4d79825a67"
   }
 };
 
